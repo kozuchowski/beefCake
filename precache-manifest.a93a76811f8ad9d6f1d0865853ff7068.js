c33a4ee416a73527459362e5a60148608b3ce7d5
Kozuchowski <marekkozuchowski@vp.pl>
2020-10-19 15:30:24 +0200

    Updates

---
+++ precache-manifest.a93a76811f8ad9d6f1d0865853ff7068.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65e7d53f4906e8a9ccd880d2800eac18",
+    "url": "/beefCake/index.html"
+  },
+  {
+    "revision": "d85dd8df240b635311f1",
+    "url": "/beefCake/static/css/main.7d83c394.chunk.css"
+  },
+  {
+    "revision": "631c11b36e9db280f111",
+    "url": "/beefCake/static/js/2.f6114b5e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/beefCake/static/js/2.f6114b5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d85dd8df240b635311f1",
+    "url": "/beefCake/static/js/main.5ef8fe86.chunk.js"
+  },
+  {
+    "revision": "b5bc7273b3fa3a377d12",
+    "url": "/beefCake/static/js/runtime-main.96f47b97.js"
+  }
+]);
